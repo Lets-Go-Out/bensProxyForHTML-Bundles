6382eb3cbfaff071bd277969c4a16a5ff4844c27
EC2 Default User <ec2-user@ip-172-31-30-19.us-west-2.compute.internal>
2019-05-03 02:50:19 +0000

    server set up to be serving the html file

--- server/index.js
+++ server/index.js
@@ -1,12 +1,15 @@
 const http = require('http');
 const fs = require('fs');
 
+require('newrelic');
+
 const dotenv = require('dotenv');
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
 http.createServer((req,res) => {
+  console.log('hello')
   if(req.url === '/') {
     fs.readFile('/home/ec2-user/public/index.html', (err, page) => {
       if(err) {
@@ -18,6 +21,14 @@ http.createServer((req,res) => {
         });
         res.end(page);
       }
+    })
+  } else if(req.url === '/loaderio-5cc233da14a1ba767f2e9ba920daa343.txt') {
+    fs.readFile('/home/ec2-user/server/loader.txt', (err, txt) => {
+      	res.writeHead(200,{
+          'Access-Control-Allow-Origin': '*',
+          'Content-Type': 'text/html'
+        });
+	res.end(txt);
     })
   }
 }).listen(PORT, () => console.log(`LISTENING ON ${PORT}`));
