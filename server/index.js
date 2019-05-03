3e4acf3c36588f797933d6b2b1b42e9a88d4ce6c
Ben Poling <benpoling@Bens-MacBook-Pro.local>
2019-05-03 11:54:10 -0700

    implemented eslint since written on server in nano

--- server/index.js
+++ server/index.js
@@ -1,33 +1,35 @@
-const http = require('http');
-const fs = require('fs');
+const http = require("http");
+const fs = require("fs");
 
-require('newrelic');
+require("newrelic");
 
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
-http.createServer((req,res) => {
-  if(req.url === '/') {
-    fs.readFile('/home/ec2-user/public/index.html', (err, page) => {
-      if(err) {
-        console.log(err);
-      } else {
-        res.writeHead(200,{
-          'Access-Control-Allow-Origin': '*',
-	  'Content-Type': 'text/html'
+http
+  .createServer((req, res) => {
+    if (req.url === "/") {
+      fs.readFile("/home/ec2-user/public/index.html", (err, page) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.writeHead(200, {
+            "Access-Control-Allow-Origin": "*",
+            "Content-Type": "text/html"
+          });
+          res.end(page);
+        }
+      });
+    } else if (req.url === "/loaderio-5cc233da14a1ba767f2e9ba920daa343.txt") {
+      fs.readFile("/home/ec2-user/server/loader.txt", (err, txt) => {
+        res.writeHead(200, {
+          "Access-Control-Allow-Origin": "*",
+          "Content-Type": "text/html"
         });
-        res.end(page);
-      }
-    })
-  } else if(req.url === '/loaderio-5cc233da14a1ba767f2e9ba920daa343.txt') {
-    fs.readFile('/home/ec2-user/server/loader.txt', (err, txt) => {
-      	res.writeHead(200,{
-          'Access-Control-Allow-Origin': '*',
-          'Content-Type': 'text/html'
-        });
-	res.end(txt);
-    })
-  }
-}).listen(PORT, () => console.log(`LISTENING ON ${PORT}`));
+        res.end(txt);
+      });
+    }
+  })
+  .listen(PORT, () => console.log(`LISTENING ON ${PORT}`));
